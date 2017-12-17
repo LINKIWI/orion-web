ebfac5abc54e51f9480a1932fc18312c25393ebf
Kevin Lin <developer@kevinlin.info>
2017-12-17 15:49:29 -0600

    Root container

--- src/app/react/containers/root.js
+++ src/app/react/containers/root.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import Root from 'app/react/components/root';
+import { Spacing } from 'react-elemental';
+import ControlPanelContainer from 'app/react/containers/control-panel';
+import HeaderContainer from 'app/react/containers/header';
+import MapContainer from 'app/react/containers/map';
 
+/**
+ * Application root container.
+ */
 const RootContainer = () => (
   <div>
     <Helmet>
-      <title>Root</title>
+      <title>Orion</title>
     </Helmet>
 
-    <Root />
+    <HeaderContainer />
+
+    <div style={{ width: '100%', height: '100vh' }}>
+      <MapContainer />
+
+      <Spacing top right style={{ backgroundColor: 'white', position: 'absolute', right: 0 }}>
+        <ControlPanelContainer />
+      </Spacing>
+    </div>
   </div>
 );
 
