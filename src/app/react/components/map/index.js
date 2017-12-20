679519c29c7511cce16fef41e631ed8d3ab86604
Kevin Lin <developer@kevinlin.info>
2017-12-19 18:34:15 -0800

    Update viewport dimensions on first render

--- src/app/react/components/map/index.js
+++ src/app/react/components/map/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Spacing } from 'react-elemental';
 import DeckGL from 'deck.gl';
@@ -7,39 +7,52 @@ import MapGL, { NavigationControl } from 'react-map-gl';
 /**
  * Wrapper around MapGL and the DeckGL overlay layer.
  */
-const MapRoot = ({ layersThunk, width, height, viewport, onViewportChange }) => (
-  <MapGL
-    mapStyle="mapbox://styles/mapbox/dark-v9"
-    mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
-    onViewportChange={onViewportChange}
-    width={width}
-    height={height}
-    {...viewport}
-  >
-    <div style={{ bottom: 0, position: 'absolute', right: 0 }}>
-      <Spacing right>
-        <Spacing size="large" bottom>
-          <NavigationControl onViewportChange={onViewportChange} />
-        </Spacing>
-      </Spacing>
-    </div>
+export default class MapRoot extends Component {
+  static propTypes = {
+    viewport: PropTypes.object.isRequired,
+    layersThunk: PropTypes.func.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    onViewportChange: PropTypes.func.isRequired,
+  };
 
-    <DeckGL
-      // Lazy evaluation - need to ensure that this is updated on re-renders.
-      layers={layersThunk()}
-      width={width}
-      height={height}
-      {...viewport}
-    />
-  </MapGL>
-);
+  componentDidMount() {
+    const { width, height, viewport, onViewportChange } = this.props;
 
-MapRoot.propTypes = {
-  viewport: PropTypes.object.isRequired,
-  layersThunk: PropTypes.func.isRequired,
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired,
-  onViewportChange: PropTypes.func.isRequired,
-};
+    // During state initialization, the store has no knowledge of the viewport dimensions since the
+    // map had not been rendered yet. Immediately after rendering the map, update the viewport state
+    // with the now-known dimensions.
+    onViewportChange({ ...viewport, width, height });
+  }
 
-export default MapRoot;
+  render() {
+    const { layersThunk, width, height, viewport, onViewportChange } = this.props;
+
+    return (
+      <MapGL
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
+        onViewportChange={onViewportChange}
+        width={width}
+        height={height}
+        {...viewport}
+      >
+        <div style={{ bottom: 0, position: 'absolute', right: 0 }}>
+          <Spacing right>
+            <Spacing size="large" bottom>
+              <NavigationControl onViewportChange={onViewportChange} />
+            </Spacing>
+          </Spacing>
+        </div>
+
+        <DeckGL
+          // Lazy evaluation - need to ensure that this is updated on re-renders.
+          layers={layersThunk()}
+          width={width}
+          height={height}
+          {...viewport}
+        />
+      </MapGL>
+    );
+  }
+}
