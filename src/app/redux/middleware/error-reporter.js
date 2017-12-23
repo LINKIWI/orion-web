14f040dd2831e5ab9a0d36317632b277b71f912e
Kevin Lin <developer@kevinlin.info>
2017-12-23 10:35:06 -0800

    Redux error reporting middleware

---
+++ src/app/redux/middleware/error-reporter.js
@@ -0,0 +1,23 @@
+import Raven from 'raven-js';
+
+/**
+ * Sentry reporting middleware for errors that are thrown when executing Redux reducers in response
+ * to dispatched actions. Since the error is explicitly try-caught, the error will not be duplicated
+ * by the globally monkey-patched exception handler via Raven.config(...).install().
+ */
+const errorReporter = (store) => (next) => (action) => {
+  try {
+    return next(action);
+  } catch (err) {
+    Raven.captureException(err, {
+      extra: {
+        action,
+        store: store.getState(),
+      },
+    });
+
+    throw err;
+  }
+};
+
+export default errorReporter;
--- src/app/redux/middleware/index.js
+++ src/app/redux/middleware/index.js
@@ -1,6 +1,8 @@
+import errorReporter from 'app/redux/middleware/error-reporter';
 import locationMiddleware from 'app/redux/middleware/location';
 
 const middleware = [
+  errorReporter,
   locationMiddleware,
 ];
 
