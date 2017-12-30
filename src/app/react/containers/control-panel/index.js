ef677e3df30aa8a3dc1735ab476d9305a252bd92
Kevin Lin <developer@kevinlin.info>
2017-12-30 15:34:15 -0800

    Handle control panel expansion logic when compact

--- src/app/react/containers/control-panel/index.js
+++ src/app/react/containers/control-panel/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Spacing } from 'react-elemental';
 import DataSourceContainer from 'app/react/containers/control-panel/data-source';
 import FiltersContainer from 'app/react/containers/control-panel/filters';
@@ -9,43 +10,86 @@ import OptionsContainer from 'app/react/containers/control-panel/options';
  * Layout container for all control panel elements.
  */
 class ControlPanelContainer extends Component {
-  state = { isExpanded: true };
+  static propTypes = {
+    isCompact: PropTypes.bool.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // Default to a non-expanded state if the device is compact
+      isExpanded: !props.isCompact,
+    };
+  }
 
   toggleExpansionState = () => this.setState(({ isExpanded }) => ({ isExpanded: !isExpanded }));
 
   render() {
+    const { isCompact } = this.props;
     const { isExpanded } = this.state;
+
+    const baseStyle = {
+      backgroundColor: 'white',
+      maxHeight: '110px',
+      opacity: 0.5,
+      overflowX: 'hidden',
+      overflowY: 'hidden',
+      transition: 'all 0.3s ease',
+      width: '500px',
+    };
+
+    const expandedStyle = {
+      maxHeight: '100vh',
+      opacity: 1,
+    };
+
+    const compactStyle = {
+      boxSizing: 'border-box',
+      maxHeight: '40px',
+      height: '100vh',
+      overflowX: 'hidden',
+      overflowY: 'auto',
+      width: 'inherit',
+    };
 
     return (
       <Spacing
         size="large"
         style={{
-          backgroundColor: 'white',
-          maxHeight: isExpanded ? '100vh' : '40px',
-          opacity: isExpanded ? 1 : 0.5,
-          overflow: 'hidden',
-          transition: 'all 0.4s ease',
-          width: '500px',
+          ...baseStyle,
+          ...isCompact && compactStyle,
+          ...isExpanded && expandedStyle,
         }}
-        top
         right
-        bottom
         left
         padding
       >
-        <Spacing size="large" bottom>
-          <Header isExpanded={isExpanded} onExpandClick={this.toggleExpansionState} />
-        </Spacing>
+        <Spacing
+          size={(isCompact && !isExpanded) ? 'tiny' : 'large'}
+          style={{ transition: 'all 0.15s ease' }}
+          top
+          bottom
+          padding
+        >
+          <Spacing size="large" bottom>
+            <Header
+              isExpanded={isExpanded}
+              isCompact={isCompact}
+              onExpandClick={this.toggleExpansionState}
+            />
+          </Spacing>
 
-        <Spacing size="large" bottom>
-          <DataSourceContainer />
-        </Spacing>
+          <Spacing size="large" bottom>
+            <DataSourceContainer />
+          </Spacing>
 
-        <Spacing size="large" bottom>
-          <FiltersContainer />
-        </Spacing>
+          <Spacing size="large" bottom>
+            <FiltersContainer />
+          </Spacing>
 
-        <OptionsContainer />
+          <OptionsContainer />
+        </Spacing>
       </Spacing>
     );
   }
