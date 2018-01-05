07df6f1d9b9349183e54c9057425c73d1a791de9
Kevin Lin <developer@kevinlin.info>
2018-01-05 09:12:08 -0600

    a11y semantics: use <a> for header expansion/contraction button

--- src/app/react/components/control-panel/header.js
+++ src/app/react/components/control-panel/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { colors } from 'react-elemental';
 import KeyboardArrowUp from 'react-icons/lib/md/keyboard-arrow-up';
@@ -7,33 +7,42 @@ import Logo from 'app/react/components/logo';
 /**
  * Control panel header exposing ability to collapse the entire panel.
  */
-const Header = ({ isExpanded, isCompact, onExpandClick }) => (
-  <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between' }}>
-    <Logo
-      style={{
-        height: (isCompact && !isExpanded) ? '20px' : '40px',
-        transition: 'all 0.15s ease',
-        width: 'auto',
-      }}
-    />
+export default class Header extends Component {
+  static propTypes = {
+    isExpanded: PropTypes.bool.isRequired,
+    isCompact: PropTypes.bool.isRequired,
+    onExpandClick: PropTypes.func.isRequired,
+  };
 
-    <div onClick={onExpandClick} style={{ cursor: 'pointer' }}>
-      <KeyboardArrowUp
-        style={{
-          color: colors.gray50,
-          fontSize: '24px',
-          transform: `rotate(${isExpanded ? 0 : 180}deg)`,
-          transition: 'all 0.3s ease',
-        }}
-      />
-    </div>
-  </div>
-);
+  handleExpansionToggleClick = (evt) => {
+    evt.preventDefault();
+    this.props.onExpandClick();
+  };
 
-Header.propTypes = {
-  isExpanded: PropTypes.bool.isRequired,
-  isCompact: PropTypes.bool.isRequired,
-  onExpandClick: PropTypes.func.isRequired,
-};
+  render() {
+    const { isExpanded, isCompact } = this.props;
 
-export default Header;
+    return (
+      <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between' }}>
+        <Logo
+          style={{
+            height: (isCompact && !isExpanded) ? '20px' : '40px',
+            transition: 'all 0.15s ease',
+            width: 'auto',
+          }}
+        />
+
+        <a href="#" onClick={this.handleExpansionToggleClick}>
+          <KeyboardArrowUp
+            style={{
+              color: colors.gray50,
+              fontSize: '24px',
+              transform: `rotate(${isExpanded ? 0 : 180}deg)`,
+              transition: 'all 0.3s ease',
+            }}
+          />
+        </a>
+      </div>
+    );
+  }
+}
