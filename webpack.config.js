7ee34e3751e9232e3f5a06780532756fffa9a6a8
Kevin Lin <developer@kevinlin.info>
2018-03-28 17:28:44 -0500

    Match webpack config of react-static-boilerplate@8b7ac69aa48b8e5a37ab72e18248c33bb9fa5d12

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ dotenv.config();
 // Array of known environment variables whose value may be injected into the frontend as a key in
 // process.env, allowing Node-like environment variable access in client-side logic.
 const BUILD_ENV_VARS = [
+  'NODE_ENV',
   'MAPBOX_API_TOKEN',
   'ORION_SERVER_URL',
   'PIWIK_URL',
@@ -69,21 +70,19 @@ module.exports = {
   },
   plugins: [
     new webpack.ProgressPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': BUILD_ENV_VARS
+        .filter((key) => key in process.env)
+        .reduce((acc, key) => ({
+          ...acc,
+          [key]: JSON.stringify(process.env[key]),
+        }), {}),
+    }),
     new HTMLWebpackPlugin({
       template: 'src/resources/templates/index.html',
       inlineSource: '.js$',
     }),
     new HtmlWebpackInlineSourcePlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        ...BUILD_ENV_VARS
-          .filter((key) => key in process.env)
-          .reduce((acc, key) => ({
-            ...acc,
-            [key]: JSON.stringify(process.env[key]),
-          }), {}),
-      },
-    }),
     isProduction && new webpack.LoaderOptionsPlugin({
       minimize: true,
     }),
