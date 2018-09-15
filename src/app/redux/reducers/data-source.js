8c18dce19a2199c4c53e1145881d362a05018cd5
Kevin Lin <developer@kevinlin.info>
2018-09-15 08:40:29 -0700

    Set default start/end timestamps to beginning of the current day and next day

--- src/app/redux/reducers/data-source.js
+++ src/app/redux/reducers/data-source.js
@@ -6,13 +6,22 @@ import {
 } from 'app/redux/actions/data-source';
 import createReducer from 'app/redux/reducers/create-reducer';
 
-const initialState = {
-  user: null,
-  device: null,
-  timestamp: {
-    start: (new Date().getTime() / 1000) - (24 * 3600),
-    end: (new Date().getTime() / 1000) + (24 * 3600),
-  },
+const initialStateFactory = () => {
+  // By default, set the start timestamp to the beginning of the current day, in local time.
+  const now = new Date();
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate()).getTime() / 1000;
+
+  // By default, set the end timestamp to the beginning of the next day, in local time.
+  const tomorrow = today + (24 * 60 * 60);
+
+  return {
+    user: null,
+    device: null,
+    timestamp: {
+      start: today,
+      end: tomorrow,
+    },
+  };
 };
 
 const setUserReducer = (state, action) => ({
@@ -48,4 +57,4 @@ const reducerMapping = {
   [SET_TIMESTAMP_END]: setTimestampEndReducer,
 };
 
-export default createReducer(reducerMapping, initialState);
+export default createReducer(reducerMapping, initialStateFactory());
